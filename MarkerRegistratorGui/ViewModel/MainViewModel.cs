80b7cd9fb53ff0bad03bdacf97c12304e49fd879
Serget Kamin <kamin@phygitalism.com>
2018-10-01 18:22:44 +0300

    Extracted tracking drom MainViewModel and added dispose

--- MarkerRegistratorGui/ViewModel/MainViewModel.cs
+++ MarkerRegistratorGui/ViewModel/MainViewModel.cs
@@ -1,90 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Reactive.Linq;
-using System.Threading;
+﻿using System.Diagnostics;
+using System.Windows.Threading;
 using MarkerRegistratorGui.Model;
 
 namespace MarkerRegistratorGui.ViewModel
 {
-	public class MainViewModel : IDisposable
+	public class MainViewModel
 	{
-		private readonly Dictionary<int, TrackedMarkerViewModel> _markers = new Dictionary<int, TrackedMarkerViewModel>();
 		private readonly ModelRoot _modelRoot = new ModelRoot()
 		{
 			TrackingService = new TuioTrackingService(),
 			RegistrationService = new DummyRegistrationService()
 		};
 
-		public ObservableCollection<TrackedMarkerViewModel> TrackedMarkers { get; }
-			= new ObservableCollection<TrackedMarkerViewModel>();
-
 		public ScaleAdapter ScaleAdapter { get; }
 		public MarkerRegistrationViewModel MarkerRegistration { get; }
-
-		private readonly List<IDisposable> _disposables;
+		public MarkerTrackingViewModel MarkerTracking { get; }
 
 		public MainViewModel()
 		{
 			ScaleAdapter = new ScaleAdapter();
 			MarkerRegistration = new MarkerRegistrationViewModel(_modelRoot.RegistrationService, ScaleAdapter);
+			MarkerTracking = new MarkerTrackingViewModel(_modelRoot.TrackingService, ScaleAdapter);
 
-
-			_disposables = new List<IDisposable>()
-			{
-				Observable.FromEvent<int>(
-					handler => _modelRoot.TrackingService.OnMarkerDown += handler,
-					handler => _modelRoot.TrackingService.OnMarkerDown -= handler
-				)
-				.ObserveOn(SynchronizationContext.Current)
-				.Subscribe(HandleMarkerDown),
-
-				Observable.FromEvent<MarkerState>(
-					handler => _modelRoot.TrackingService.OnMarkerStateUpdate += handler,
-					handler => _modelRoot.TrackingService.OnMarkerStateUpdate -= handler
-				)
-				.Subscribe(HandleMarkerUpdate),
-
-				Observable.FromEvent<int>(
-					handler => _modelRoot.TrackingService.OnMarkerUp += handler,
-					handler => _modelRoot.TrackingService.OnMarkerUp -= handler
-				)
-				.ObserveOn(SynchronizationContext.Current)
-				.Subscribe(HandleMarkerUp),
-			};
+			Dispatcher.CurrentDispatcher.ShutdownStarted += (sender, e) => Dispose();
 
 			_modelRoot.TrackingService.Start();
 		}
 
-		private void HandleMarkerDown(int id)
-		{
-			if (_markers.ContainsKey(id))
-				HandleMarkerUp(id);
-
-			var marker = new TrackedMarkerViewModel(id, ScaleAdapter);
-
-			_markers.Add(id, marker);
-			TrackedMarkers.Add(marker);
-		}
-
-		private void HandleMarkerUp(int id)
-		{
-			if (_markers.TryGetValue(id, out var marker))
-			{
-				_markers.Remove(id);
-				TrackedMarkers.Remove(marker);
-			}
-		}
-
-		private void HandleMarkerUpdate(MarkerState state)
-		{
-			if (_markers.TryGetValue(state.id, out var marker))
-				marker.UpdateValues(state.position, state.rotation, state.radius);
-		}
-
 		public void Dispose()
 		{
-			_disposables.ForEach(d => d.Dispose());
+			Debug.WriteLine("Disposing");
 			_modelRoot.TrackingService.Stop();
 		}
 	}
---
+++ MarkerRegistratorGui/ViewModel/MarkerTrackingViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Threading;
+using MarkerRegistratorGui.Model;
+
+namespace MarkerRegistratorGui.ViewModel
+{
+	public class MarkerTrackingViewModel : IDisposable
+	{
+		private readonly Dictionary<int, TrackedMarkerViewModel> _markers = new Dictionary<int, TrackedMarkerViewModel>();
+		private readonly List<IDisposable> _disposables;
+		private readonly ScaleAdapter _scaleAdapter;
+
+		public ObservableCollection<TrackedMarkerViewModel> TrackedMarkers { get; }
+			= new ObservableCollection<TrackedMarkerViewModel>();
+
+		public MarkerTrackingViewModel(IMarkerTrackingService markerTracking, ScaleAdapter scaleAdapter)
+		{
+			_scaleAdapter = scaleAdapter;
+
+			_disposables = new List<IDisposable>()
+			{
+				Observable.FromEvent<int>(
+					handler => markerTracking.OnMarkerDown += handler,
+					handler => markerTracking.OnMarkerDown -= handler
+				)
+				.ObserveOn(SynchronizationContext.Current)
+				.Subscribe(HandleMarkerDown),
+
+				Observable.FromEvent<MarkerState>(
+					handler => markerTracking.OnMarkerStateUpdate += handler,
+					handler => markerTracking.OnMarkerStateUpdate -= handler
+				)
+				.Subscribe(HandleMarkerUpdate),
+
+				Observable.FromEvent<int>(
+					handler => markerTracking.OnMarkerUp += handler,
+					handler => markerTracking.OnMarkerUp -= handler
+				)
+				.ObserveOn(SynchronizationContext.Current)
+				.Subscribe(HandleMarkerUp),
+			};
+		}
+
+		private void HandleMarkerDown(int id)
+		{
+			if (_markers.ContainsKey(id))
+				HandleMarkerUp(id);
+
+			var marker = new TrackedMarkerViewModel(id, _scaleAdapter);
+
+			_markers.Add(id, marker);
+			TrackedMarkers.Add(marker);
+		}
+
+		private void HandleMarkerUp(int id)
+		{
+			if (_markers.TryGetValue(id, out var marker))
+			{
+				_markers.Remove(id);
+				TrackedMarkers.Remove(marker);
+			}
+		}
+
+		private void HandleMarkerUpdate(MarkerState state)
+		{
+			if (_markers.TryGetValue(state.id, out var marker))
+				marker.UpdateValues(state.position, state.rotation, state.radius);
+		}
+
+		public void Dispose() => _disposables.ForEach(d => d.Dispose());
+	}
+}
