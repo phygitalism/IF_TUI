13283213d47d47874fdfb8b230b06fc6aa7689c1
Sergey Polischouck <polischouckserg@gmail.com>
2018-10-01 15:21:15 +0300

    Add separate project for recognition service

---
+++ RecognitionService/MenuViewController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace RecognitionService
+{
+    public class MenuViewController
+    {
+        public MenuViewController(IDeviceController deviceController)
+        {
+            System.Diagnostics.Debug.Assert(deviceController != null);
+
+            _deviceController = deviceController;
+
+            _components = new System.ComponentModel.Container();
+            _notifyIcon = new System.Windows.Forms.NotifyIcon(_components)
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = RecognitionService.Properties.Resources.NotReadyIcon,
+                Text = "Overlay is not found",
+                Visible = true,
+            };
+
+            // _notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+            // _notifyIcon.DoubleClick += notifyIcon_DoubleClick;
+            // _notifyIcon.MouseUp += notifyIcon_MouseUp;
+
+            //_aboutViewModel = new WpfFormLibrary.ViewModel.AboutViewModel();
+            //_statusViewModel = new WpfFormLibrary.ViewModel.StatusViewModel();
+
+            // _statusViewModel.Icon = AppIcon;
+            // _aboutViewModel.Icon = _statusViewModel.Icon;
+
+            // _hiddenWindow = new System.Windows.Window();
+            // _hiddenWindow.Hide();
+        }
+
+        System.Windows.Media.ImageSource AppIcon
+        {
+            get
+            {
+                // System.Drawing.Icon icon = (_deviceController.Status == DeviceStatus.Running) ? Properties.Resources.ReadyIcon : Properties.Resources.NotReadyIcon;
+                System.Drawing.Icon icon = Properties.Resources.NotReadyIcon;
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                    icon.Handle,
+                    System.Windows.Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+
+        // This allows code to be run on a GUI thread
+        // private System.Windows.Window _hiddenWindow;
+
+        private System.ComponentModel.IContainer _components;
+        // The Windows system tray class
+        private NotifyIcon _notifyIcon;
+        IDeviceController _deviceController;
+    }
+}
---
+++ RecognitionService/STAApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecognitionService
+{
+    public class STAApplicationContext : ApplicationContext
+    {
+        private TouchOverlay _touchOverlay;
+        private MenuViewController _menuViewController;
+
+        public STAApplicationContext()
+        {
+            _touchOverlay = new TouchOverlay();
+            _menuViewController = new MenuViewController(_touchOverlay);
+        }
+
+        // Called from the Dispose method of the base class
+        protected override void Dispose(bool disposing)
+        {
+            _touchOverlay = null;
+            _menuViewController = null;
+        }
+    }
+}
