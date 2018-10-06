bed31c57d5727a47c62dc87d39112b2ec62af11a
kamin <kamin@phygitalism.com>
2018-10-07 01:16:56 +0300

    Added basic pointer injector

---
+++ MarkerRegistratorGui/Model/PointerInjector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MarkerRegistratorGui.Model
+{
+	public static class PointerInjector
+	{
+		static PointerInjector()
+		{
+			if (!TouchInjector.InitializeTouchInjection(255, TouchFeedback.INDIRECT))
+				throw new Exception("Couldn't initialize touch injection");
+		}
+
+		public static void InjectPointerDown(int id, int x, int y)
+			=> InjectPointer(id, x, y, PointerFlags.INRANGE | PointerFlags.DOWN);
+
+		public static void InjectPointerUpdate(int id, int x, int y)
+			=> InjectPointer(id, x, y, PointerFlags.INRANGE | PointerFlags.UPDATE);
+
+		public static void InjectPointerUp(int id, int x, int y)
+			=> InjectPointer(id, x, y, PointerFlags.INRANGE | PointerFlags.UP);
+
+		private static void InjectPointer(int id, int x, int y, PointerFlags flags)
+		{
+			var pointerInfo = new PointerTouchInfo()
+			{
+				PointerInfo =
+				{
+					PointerId = (uint)id,
+					PointerType = PointerInputType.TOUCH,
+					PointerFlags = flags,
+					PtPixelLocation =
+					{
+						X = x,
+						Y = y
+					}
+				}
+			};
+
+			if (!TouchInjector.InjectTouchInput(1, new[] { pointerInfo }))
+				throw new Exception("Couldn't inject");
+		}
+	}
+}
