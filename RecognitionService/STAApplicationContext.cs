69df3054d5ac767799ba048ab72ce41992e23e10
Sergey Polischouck <polischouckserg@maas.agency>
2018-10-12 14:46:03 +0300

    add ws server with tangible marker controller

--- RecognitionService/STAApplicationContext.cs
+++ RecognitionService/STAApplicationContext.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using RecognitionService.Api;
+
 namespace RecognitionService
 {
     public class STAApplicationContext : ApplicationContext
     {
-        private IDeviceController _deviceController;
+		private const int _serverPort = 8080;
+
+		private IDeviceController _deviceController;
         private MenuViewController _menuViewController;
         private InputSerializer _inputSerializer;
         private InputLogger _inputLogger;
@@ -17,9 +21,18 @@ namespace RecognitionService
 
         private TouchPointFrameGenerator _touchPointFrameGenerator;
 
-        public STAApplicationContext()
+		private RecognitionServiceServer _wsServer;
+
+		private TangibleMarkerController _tangibleMarkerController = new TangibleMarkerController();
+
+
+		public STAApplicationContext()
         {
-            var isDeviceMocked = false;
+			_wsServer = new RecognitionServiceServer(_serverPort);
+
+			_wsServer.OnMarkerListRequested += _tangibleMarkerController.GetAllRegistredIds;
+
+			var isDeviceMocked = false;
             if (!isDeviceMocked)
             {
                 var touchOverlay = new TouchOverlay();
@@ -28,10 +41,10 @@ namespace RecognitionService
                 _inputSerializer = new InputSerializer(touchOverlay);
                 _inputLogger = new InputLogger(touchOverlay);
 
-                _touchPointFrameGenerator = new TouchPointFrameGenerator();                
-                _tuioServer = new TuioServer(touchOverlay);
-                // _tuioServer = new TuioServer(_touchPointFrameGenerator);
-            }
+                //_touchPointFrameGenerator = new TouchPointFrameGenerator();
+				_tuioServer = new TuioServer(touchOverlay);
+				//_tuioServer = new TuioServer(_touchPointFrameGenerator);
+			}
             else
             {
                 _deviceController = (IDeviceController)new DeviceMock();
@@ -47,7 +60,12 @@ namespace RecognitionService
         // Called from the Dispose method of the base class
         protected override void Dispose(bool disposing)
         {
-            if (_touchPointFrameGenerator != null)
+			if (_wsServer != null)
+			{
+				_wsServer.Dispose();
+				_wsServer = null;
+			}
+			if (_touchPointFrameGenerator != null)
             {
                 _touchPointFrameGenerator.Dispose();
                 _touchPointFrameGenerator = null;
---
+++ RecognitionService/TangibleMarkerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RecognitionService.Models;
+
+namespace RecognitionService
+{
+	class TangibleMarkerController
+	{
+
+		public MarkerConfig Config { get; private set; }
+
+		private Storage _storage = new Storage();
+
+		public TangibleMarkerController()
+		{
+			Config = _storage.Load();
+		}
+
+		public void RegisterMarkerWithId(Triangle triangle, int id)
+		{
+			var tangible = new RegistredTangibleMarker(id, triangle, 0.0f, 0.0f);
+			Config.Add(tangible);
+			_storage.Save(Config);
+		}
+
+		public void UnregisterMarkerWithId(int id)
+		{
+			var tangible = Config.registredTangibles.Find(marker => marker.Id == id);
+			Config.Remove(tangible);
+			_storage.Save(Config);
+		}
+
+		public int[] GetAllRegistredIds()
+		{
+			Console.WriteLine("OnMarkerListRequested");
+			return Config.registredTangibles.Select(marker => marker.Id).ToArray();
+		}
+	}
+}
