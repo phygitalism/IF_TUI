5279a1542dc32bf9bda0b571aeba6777f7b2249f
Sergey Kamin <kamin@phygitalism.com>
2018-12-12 02:42:43 +0300

    Made screen scaler thread agnostic

--- MarkerRegistratorGui/View/ScreenScaler.cs
+++ MarkerRegistratorGui/View/ScreenScaler.cs
@@ -1,5 +1,8 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace MarkerRegistratorGui.View
 {
@@ -7,9 +10,51 @@ namespace MarkerRegistratorGui.View
 	{
 		private readonly Window _window;
 
+		private (double X, double Y) _scale;
+		private (double Width, double Height) _size;
+		private (double X, double Y) _position;
+
 		public ScreenScaler(Window window)
 		{
 			_window = window;
+
+			_window.Loaded += (sender, args) =>
+			{
+				UpdateScale((Window)sender);
+				UpdateSizeAndPos((Window)sender);
+			};
+			_window.SizeChanged += (sender, args) => UpdateSizeAndPos((Window)sender);
+			_window.LocationChanged += (sender, args) => UpdateSizeAndPos((Window)sender);
+		}
+
+		private void UpdateSizeAndPos(Window window)
+		{
+			_position = (window.Left, window.Top);
+			_size = (window.Width, window.Height);
+
+			Debug.WriteLine($"Position = {_position}");
+			Debug.WriteLine($"Size = {_size}");
+		}
+
+		private void UpdateScale(Window window)
+		{
+			Matrix matrix;
+			var source = PresentationSource.FromVisual(window);
+			if (source != null)
+			{
+				matrix = source.CompositionTarget.TransformToDevice;
+			}
+			else
+			{
+				using (var src = new HwndSource(new HwndSourceParameters()))
+				{
+					matrix = src.CompositionTarget.TransformToDevice;
+				}
+			}
+
+			_scale = (matrix.M11, matrix.M22);
+
+			Debug.WriteLine($"Scale = {_scale}");
 		}
 
 		public (int x, int y) ScaleAndSafePosition(Vector2 position)
@@ -17,14 +62,15 @@ namespace MarkerRegistratorGui.View
 			var unscaledX = position.X;
 			var unscaledY = position.Y;
 
-			var scaledX = unscaledX * _window.Width;
-			var scaledY = unscaledY * _window.Height;
+			var scaledX = unscaledX * _size.Width * _scale.X;
+			var scaledY = unscaledY * _size.Height * _scale.Y;
 
-			var screenPoint = _window.PointToScreen(new Point(scaledX, scaledY));
+			var absoluteX = _position.X + scaledX;
+			var absoluteY = _position.Y + scaledY;
 
 			return (
-				SizeSafe((int)screenPoint.X, unscaledX),
-				SizeSafe((int)screenPoint.Y, unscaledY)
+				SizeSafe((int)absoluteX, unscaledX),
+				SizeSafe((int)absoluteY, unscaledY)
 			);
 		}
 
